125579fdbdd0daad3e1ff2c005a8d9c61593f3d3
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-12-03 17:40:12 -0500

    TaskListService progress

--- Taskmato-2/Data/Services/TaskListService.cs
+++ Taskmato-2/Data/Services/TaskListService.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Taskmato_2.Models;
 
 namespace Taskmato_2.Data.Services
 {
     public class TaskListService
     {
+        private TaskmatoContext context;
+        private DbSet<User> users;
+        private DbSet<TaskList> taskLists;
+
+        public TaskListService(TaskmatoContext context)
+        {
+            this.context = context;
+            users = context.TaskmatoUsers;
+            taskLists = context.TaskLists;
+        }
     }
 }
