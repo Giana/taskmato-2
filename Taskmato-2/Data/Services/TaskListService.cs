dc7866d89e89a415064fb0ee0fa618ce5a592351
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-12-03 19:07:41 -0500

    Completed TaskListService

--- Taskmato-2/Data/Services/TaskListService.cs
+++ Taskmato-2/Data/Services/TaskListService.cs
@@ -9,15 +9,40 @@ namespace Taskmato_2.Data.Services
 {
     public class TaskListService
     {
-        private TaskmatoContext context;
-        private DbSet<User> users;
-        private DbSet<TaskList> taskLists;
+        private TaskmatoContext Context;
+        private DbSet<User> Users;
+        private DbSet<TaskList> TaskLists;
 
         public TaskListService(TaskmatoContext context)
         {
-            this.context = context;
-            users = context.TaskmatoUsers;
-            taskLists = context.TaskLists;
+            Context = context;
+            Users = context.TaskmatoUsers;
+            TaskLists = context.TaskLists;
+        }
+
+        public void AddTaskList(TaskList taskList)
+        {
+            TaskLists.Add(taskList);
+        }
+
+        public void DeleteTaskList(int taskListId)
+        {
+            TaskList TaskList = TaskLists.FirstOrDefault(x => x.TaskListId == taskListId);
+            TaskLists.Remove(TaskList);
+        }
+
+        public TaskList RetrieveTaskList(int taskListId)
+        {
+            TaskList TaskList = TaskLists.Include(x => x.Taskmatos).FirstOrDefault(x => x.TaskListId == taskListId);
+
+            return TaskList;
+        }
+
+        public ICollection<TaskList> RetrieveTaskLists(int userId)
+        {
+            User User = Users.Include(x => x.TaskLists).ThenInclude(x => x.Taskmatos).FirstOrDefault(x => x.UserId == userId);
+
+            return User.TaskLists;
         }
     }
 }
