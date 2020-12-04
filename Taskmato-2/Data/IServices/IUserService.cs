afd3182d3f8fd153537f2107507b947d6058fa4a
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-12-04 11:40:10 -0500

    Created interfaces (skeletons) for services (forgot to create them previously)

---
+++ Taskmato-2/Data/IServices/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taskmato_2.Models;
+
+namespace Taskmato_2.Data.IServices
+{
+    public interface IUserService
+    {
+    }
+}
