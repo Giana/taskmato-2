392ddc8ad27467165466c474cdfef05f8d136845
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-12-03 22:36:48 -0500

    TaskmatoService method skeletons

--- Taskmato-2/Data/Services/TaskmatoService.cs
+++ Taskmato-2/Data/Services/TaskmatoService.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Taskmato_2.Models;
 
 namespace Taskmato_2.Data.Services
 {
     public class TaskmatoService
     {
+        private TaskmatoContext Context;
+        private DbSet<Taskmato> Taskmatos;
+        private DbSet<TaskList> TaskLists;
+
+        public TaskmatoService(TaskmatoContext context)
+        {
+            Context = context;
+            Taskmatos = context.Taskmatos;
+            TaskLists = context.TaskLists;
+        }
+
+        public void AddTaskmato(Taskmato taskmato)
+        {
+
+        }
+
+        public void EditTaskmato(int taskmatoId, Taskmato taskmato)
+        {
+
+        }
+
+        public void DeleteTaskmato(int taskmatoId)
+        {
+
+        }
+
+        public Taskmato GetTaskmato(int taskmatoId)
+        {
+
+        }
+
+        public ICollection<Taskmato> RetrieveTaskmatos(int taskListId)
+        {
+
+        }
+
+
     }
 }
